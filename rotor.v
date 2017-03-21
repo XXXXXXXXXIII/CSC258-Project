6fdb60de454c5fcf6afc5054995072221dbcc641
Tim Xie <tim.xie@mail.utoronto.ca>
2017-03-21 16:24:00 -0400

    commit

--- rotor.v
+++ rotor.v
@@ -68,11 +68,8 @@ endmodule
 
 module rotor_wiring (in, out, wiring_config);
 	input [25:0]in;
-	input [1:0]wiring_config;
+	input [1:0]wiring_config; // Rotor I: 00, Rotor II: 01, Rotor III: 10, No Encryption: 10
 	output [25:0]out;
-
-	localparam A = 5'd0, B = 5'd1, C = 5'd2, D = 5'd3, E = 5'd4, F = 5'd5, G = 5'd6, H = 5'd7, I = 5'd8, J = 5'd9, K = 5'd10, L = 5'd11, M = 5'd12, N = 5'd13, 
-			O = 5'd14, P = 5'd15, Q = 5'd16, R = 5'd17, S = 5'd18, T = 5'd19, U = 5'd20, V = 5'd21, W = 5'd22, X = 5'd23, Y = 5'd24, Z = 5'd25;
 
 	always@(*)
 	begin
@@ -107,61 +104,61 @@ module rotor_wiring (in, out, wiring_config);
 		end
 		else if (wiring_config == 2'b01)
 		begin
-			assign out[0] = in[];
-			assign out[1] = in[];
-			assign out[2] = in[];
-			assign out[3] = in[];
-			assign out[4] = in[];
-			assign out[5] = in[];
-			assign out[6] = in[];
-			assign out[7] = in[];
-			assign out[8] = in[];
-			assign out[9] = in[];
-			assign out[10] = in[];
-			assign out[11] = in[];
-			assign out[12] = in[];
-			assign out[13] = in[];
-			assign out[14] = in[];
-			assign out[15] = in[];
-			assign out[16] = in[];
-			assign out[17] = in[];
-			assign out[18] = in[];
-			assign out[19] = in[];
-			assign out[20] = in[];
-			assign out[21] = in[];
-			assign out[22] = in[];
-			assign out[23] = in[];
-			assign out[24] = in[];
-			assign out[25] = in[];
+			assign out[0] = in[0];
+			assign out[1] = in[9];
+			assign out[2] = in[3];
+			assign out[3] = in[10];
+			assign out[4] = in[18];
+			assign out[5] = in[8];
+			assign out[6] = in[17];
+			assign out[7] = in[20];
+			assign out[8] = in[23];
+			assign out[9] = in[1];
+			assign out[10] = in[11];
+			assign out[11] = in[7];
+			assign out[12] = in[22];
+			assign out[13] = in[19];
+			assign out[14] = in[12];
+			assign out[15] = in[2];
+			assign out[16] = in[14];
+			assign out[17] = in[6];
+			assign out[18] = in[25];
+			assign out[19] = in[13];
+			assign out[20] = in[15];
+			assign out[21] = in[24];
+			assign out[22] = in[5];
+			assign out[23] = in[21];
+			assign out[24] = in[14];
+			assign out[25] = in[4];
 		end
 		else if (wiring_config == 2'b10)
 		begin
-			assign out[0] = in[4];
-			assign out[1] = in[];
-			assign out[2] = in[];
-			assign out[3] = in[];
-			assign out[4] = in[];
-			assign out[5] = in[];
-			assign out[6] = in[];
-			assign out[7] = in[];
-			assign out[8] = in[];
-			assign out[9] = in[];
-			assign out[10] = in[];
-			assign out[11] = in[];
-			assign out[12] = in[];
-			assign out[13] = in[];
-			assign out[14] = in[];
-			assign out[15] = in[];
-			assign out[16] = in[];
-			assign out[17] = in[];
-			assign out[18] = in[];
-			assign out[19] = in[];
-			assign out[20] = in[];
-			assign out[21] = in[];
-			assign out[22] = in[];
-			assign out[23] = in[];
-			assign out[24] = in[];
-			assign out[25] = in[];
+			assign out[0] = in[1];
+			assign out[1] = in[3];
+			assign out[2] = in[5];
+			assign out[3] = in[7];
+			assign out[4] = in[9];
+			assign out[5] = in[11];
+			assign out[6] = in[2];
+			assign out[7] = in[15];
+			assign out[8] = in[17];
+			assign out[9] = in[19];
+			assign out[10] = in[23];
+			assign out[11] = in[21];
+			assign out[12] = in[25];
+			assign out[13] = in[13];
+			assign out[14] = in[24];
+			assign out[15] = in[4];
+			assign out[16] = in[8];
+			assign out[17] = in[22];
+			assign out[18] = in[6];
+			assign out[19] = in[0];
+			assign out[20] = in[10];
+			assign out[21] = in[12];
+			assign out[22] = in[20];
+			assign out[23] = in[18];
+			assign out[24] = in[16];
+			assign out[25] = in[14];
 		end
 		else
 		begin
