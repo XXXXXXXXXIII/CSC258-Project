affd3457c8c947e0420adfd44f562f6dd752afc3
Tim Xie <tim.xie@mail.utoronto.ca>
2017-03-21 16:15:20 -0400

    message

--- rotor.v
+++ rotor.v
@@ -47,6 +47,7 @@ module rotor (in, out, rotate, wiring_config);
 			O = 5'd14, P = 5'd15, Q = 5'd16, R = 5'd17, S = 5'd18, T = 5'd19, U = 5'd20, V = 5'd21, W = 5'd22, X = 5'd23, Y = 5'd24, Z = 5'd25;
 
 	reg [4:0]curr_pos;
+	
 
 	always @(*)
 	begin
@@ -66,9 +67,9 @@ endmodule
 
 
 module rotor_wiring (in, out, wiring_config);
-	input [4:0]in;
+	input [25:0]in;
 	input [1:0]wiring_config;
-	output reg [4:0]out;
+	output [25:0]out;
 
 	localparam A = 5'd0, B = 5'd1, C = 5'd2, D = 5'd3, E = 5'd4, F = 5'd5, G = 5'd6, H = 5'd7, I = 5'd8, J = 5'd9, K = 5'd10, L = 5'd11, M = 5'd12, N = 5'd13, 
 			O = 5'd14, P = 5'd15, Q = 5'd16, R = 5'd17, S = 5'd18, T = 5'd19, U = 5'd20, V = 5'd21, W = 5'd22, X = 5'd23, Y = 5'd24, Z = 5'd25;
@@ -77,99 +78,119 @@ module rotor_wiring (in, out, wiring_config);
 	begin
 		if (wiring_config == 2'b00)
 		begin
-			case(in)
-				A: out = E;
-				B: out = K;
-				C: out = M;
-				D: out = F;
-				E: out = L;
-				F: out = G;
-				H: out = D;
-				H: out = Q;
-				I: out = V;
-				J: out = Z;
-				K: out = N;
-				L: out = T;
-				M: out = O;
-				N: out = W;
-				O: out = Y;
-				P: out = H;
-				Q: out = X;
-				R: out = U;
-				S: out = S;
-				T: out = P;
-				U: out = A;
-				V: out = I;
-				W: out = B;
-				X: out = R;
-				Y: out = C;
-				Z: out = J;
-				default: out = 1;
-			endcase
+			assign out[0] = in[4];
+			assign out[1] = in[10];
+			assign out[2] = in[12];
+			assign out[3] = in[5];
+			assign out[4] = in[11];
+			assign out[5] = in[6];
+			assign out[6] = in[3];
+			assign out[7] = in[16];
+			assign out[8] = in[21];
+			assign out[9] = in[25];
+			assign out[10] = in[13];
+			assign out[11] = in[19];
+			assign out[12] = in[14];
+			assign out[13] = in[22];
+			assign out[14] = in[24];
+			assign out[15] = in[7];
+			assign out[16] = in[23];
+			assign out[17] = in[20];
+			assign out[18] = in[18];
+			assign out[19] = in[15];
+			assign out[20] = in[0];
+			assign out[21] = in[8];
+			assign out[22] = in[2];
+			assign out[23] = in[17];
+			assign out[24] = in[2];
+			assign out[25] = in[9];
 		end
 		else if (wiring_config == 2'b01)
 		begin
-			case(in)
-				A: out = A;
-				B: out = J;
-				C: out = D;
-				D: out = K;
-				E: out = S;
-				F: out = I;
-				H: out = R;
-				H: out = U;
-				I: out = X;
-				J: out = B;
-				K: out = L;
-				L: out = H;
-				M: out = W;
-				N: out = T;
-				O: out = M;
-				P: out = C;
-				Q: out = Q;
-				R: out = G;
-				S: out = Z;
-				T: out = N;
-				U: out = P;
-				V: out = Y;
-				W: out = F;
-				X: out = V;
-				Y: out = O;
-				Z: out = E;
-				default: out = 1;
-			endcase
+			assign out[0] = in[];
+			assign out[1] = in[];
+			assign out[2] = in[];
+			assign out[3] = in[];
+			assign out[4] = in[];
+			assign out[5] = in[];
+			assign out[6] = in[];
+			assign out[7] = in[];
+			assign out[8] = in[];
+			assign out[9] = in[];
+			assign out[10] = in[];
+			assign out[11] = in[];
+			assign out[12] = in[];
+			assign out[13] = in[];
+			assign out[14] = in[];
+			assign out[15] = in[];
+			assign out[16] = in[];
+			assign out[17] = in[];
+			assign out[18] = in[];
+			assign out[19] = in[];
+			assign out[20] = in[];
+			assign out[21] = in[];
+			assign out[22] = in[];
+			assign out[23] = in[];
+			assign out[24] = in[];
+			assign out[25] = in[];
 		end
 		else if (wiring_config == 2'b10)
 		begin
-			case(in)
-				A: out = B;
-				B: out = D;
-				C: out = F;
-				D: out = H;
-				E: out = J;
-				F: out = L;
-				H: out = C;
-				H: out = P;
-				I: out = R;
-				J: out = T;
-				K: out = X;
-				L: out = V;
-				M: out = Z;
-				N: out = N;
-				O: out = Y;
-				P: out = E;
-				Q: out = I;
-				R: out = W;
-				S: out = G;
-				T: out = A;
-				U: out = K;
-				V: out = M;
-				W: out = U;
-				X: out = S;
-				Y: out = Q;
-				Z: out = O;
-				default: out = 1;
-			endcase
+			assign out[0] = in[4];
+			assign out[1] = in[];
+			assign out[2] = in[];
+			assign out[3] = in[];
+			assign out[4] = in[];
+			assign out[5] = in[];
+			assign out[6] = in[];
+			assign out[7] = in[];
+			assign out[8] = in[];
+			assign out[9] = in[];
+			assign out[10] = in[];
+			assign out[11] = in[];
+			assign out[12] = in[];
+			assign out[13] = in[];
+			assign out[14] = in[];
+			assign out[15] = in[];
+			assign out[16] = in[];
+			assign out[17] = in[];
+			assign out[18] = in[];
+			assign out[19] = in[];
+			assign out[20] = in[];
+			assign out[21] = in[];
+			assign out[22] = in[];
+			assign out[23] = in[];
+			assign out[24] = in[];
+			assign out[25] = in[];
+		end
+		else
+		begin
+			assign out[0] = in[0];
+			assign out[1] = in[1];
+			assign out[2] = in[2];
+			assign out[3] = in[3];
+			assign out[4] = in[4];
+			assign out[5] = in[5];
+			assign out[6] = in[6];
+			assign out[7] = in[7];
+			assign out[8] = in[8];
+			assign out[9] = in[9];
+			assign out[10] = in[10];
+			assign out[11] = in[11];
+			assign out[12] = in[12];
+			assign out[13] = in[13];
+			assign out[14] = in[14];
+			assign out[15] = in[15];
+			assign out[16] = in[16];
+			assign out[17] = in[17];
+			assign out[18] = in[18];
+			assign out[19] = in[19];
+			assign out[20] = in[20];
+			assign out[21] = in[21];
+			assign out[22] = in[22];
+			assign out[23] = in[23];
+			assign out[24] = in[24];
+			assign out[25] = in[25];
 		end
 	end
 endmodule
