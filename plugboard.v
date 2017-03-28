a2b21ebb484f33b8a9765b308f21f9bf42b01940
Tim Xie <xietian9@markus.teach.cs.toronto.edu>
2017-03-27 20:39:51 -0400

    more files

--- plugboard.v
+++ plugboard.v
@@ -1,3 +1,4 @@
+<<<<<<< df4d4ac2da9978d6a97a09903152063bfe1ec153
 module plugboardchanger(in, out, changeto, change);
     input [25:0] in;
     input [25:0] changeto;
@@ -15,3 +16,23 @@ module plugboardchanger(in, out, changeto, change);
 		out = in;
 	end
 endmodule
+=======
+module plugboardChanger(INPUT, OUTPUT, CHANGETO, CHANGE);
+
+    input [25:0] INPUT;
+    input [25:0] CHANGETO;
+    input CHANGE;
+    
+    output [25:0] OUTPUT;
+    reg OUTPUT;
+    
+    always @(*)
+	begin
+	    if (CHANGE)
+		OUTPUT = CHANGETO;
+	    else
+		OUTPUT = INPUT;
+	end
+    
+endmodule
+>>>>>>> more files
