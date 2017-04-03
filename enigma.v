0940b8255ae4999a3bed723e33979e95abf05d02
anifshikder <anif.shikder@mail.utoronto.ca>
2017-04-03 17:05:00 -0400

    keyboard

--- enigma.v
+++ enigma.v
@@ -1,10 +1,11 @@
-module enigma (CLOCK_50, KEY, SW, VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK_N, VGA_SYNC_N, VGA_R, VGA_G, VGA_B);
+module enigma (CLOCK_50, KEY, SW, VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK_N, VGA_SYNC_N, VGA_R, VGA_G, VGA_B,
+		PS2_CLK, PS2_DAT);
 
 	input CLOCK_50;
 	input [3:0]KEY;
 	input [9:0]SW;
 
-    output	VGA_CLK;   				//	VGA Clock
+	output	VGA_CLK;   				//	VGA Clock
 	output	VGA_HS;					//	VGA H_SYNC
 	output	VGA_VS;					//	VGA V_SYNC
 	output	VGA_BLANK_N;				//	VGA BLANK
@@ -15,10 +16,13 @@ module enigma (CLOCK_50, KEY, SW, VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK_N, VGA_SYNC
 
 	wire [25:0]rero_out, front_plug_out, rear_plug_out;
 	wire [4:0]state1, state2, state3;
+	
+	input PS2_CLK, PS2_DAT;
+	reg [25:0] r;
 
 
-    
-    // plugboardChanger plugboard (.in(in), .out(w1), .changeto(changeto), .change(change)); //TODO keyboard/plugboard. connect front_plug_out to output, add pin-input as needed.
+    keyboard kbd(.PS2_CLK(PS2_CLK),.PS2_DAT(PS2_DAT),.CLOCK_50(CLOCK_50), .r(r));    
+    plugboardChanger plugboard (.in(r), .change(change), .r1(front_plug_out)); //TODO keyboard/plugboard. connect front_plug_out to output, add pin-input as needed.
     rero rotors_reflector(.in(front_plug_out), .out(rero_out), .wheel_config(SW[2:0]), .rotate1(KEY[1]), .rotate2(KEY[2]), .rotate3(KEY[3]), .state1(state1), .state2(state2), .state3(state3));
     gui gui0(.CLOCK_50(CLOCK_50), .in(rear_plug_out), .state1(state1), .state2(state2), .state3(state3), .reset(KEY[0]), 
     	.VGA_CLK(VGA_CLK), .VGA_HS(VGA_HS), .VGA_VS(VGA_VS), .VGA_BLANK_N(VGA_BLANK_N), .VGA_SYNC_N(VGA_SYNC_N), .VGA_R(VGA_R), .VGA_G(VGA_G), .VGA_B(VGA_B));
@@ -26,13 +30,15 @@ module enigma (CLOCK_50, KEY, SW, VGA_CLK, VGA_HS, VGA_VS, VGA_BLANK_N, VGA_SYNC
 endmodule
 
 
-module plugboardChanger(in, change);
+module plugboardChanger(in, change, r1);
 
 	input [25:0] in, change;
 
 	reg counter;
 	reg [25:0] r1, input1, input2, input3, input4, input5, input6, input7, input8, input9, input10;
 	reg [25:0] output1, output2, output3, output4, output5, output6, output7, output8, output9, output10;
+	
+	output r1;
 
 	initial begin
 		counter <= 0;
